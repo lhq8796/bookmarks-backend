cb789e69458163046c33adb07c30b1d77c0aab0c
lhq8796 <lhq8796@outlook.com>
2021-09-04 22:33:37 +0800

    批量读取目录下的书签文件

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-var cheerio = require("cheerio"),
-  fs = require("fs");
+// var cheerio = require("cheerio"),
+//   fs = require("fs");
+// import fs from 'fs';
+import path from 'path';
+import {readdir, readFile, writeFile } from 'fs/promises';
+import cheerio from 'cheerio';
 
-// 读取书签 html 文件
-fs.readFile("favorites.html", "utf-8", function (err, data) {
-  if (err) {
-    console.log("error", err);
-  } else {
-    parse(data);
+try {
+  mergeBookmarks()
+} catch (err) {
+  console.error(err);
+}
+
+// // 读取书签 html 文件
+// fs.readFile("favorites.html", "utf-8", function (err, data) {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     parse(data);
+//   }
+// });
+async function mergeBookmarks() {
+  const files = await readdir ('./bookmarks')
+  const result = []
+  for (const file of files) {
+    const promise = readFile(path.join('bookmarks', file), 'utf-8')
+    result.push(promise)
   }
-});
+  console.log(result);
+  Promise.all(result).then(values => {
+    const result = []
+    for (const html of values) {
+      const obj = parse(html)
+      result.push(obj)
+    }
+    // 将对象转化为json字符串，添加额外参数使json格式更易阅读
+    var s = JSON.stringify(result, null, 4);
+    // 将json字符串写入json文件
+    writeFile("bookmarks.json", s);
+  })
+}
 
 function parse(html) {
   // 加载 html，使用常用的 $ 符号
@@ -21,10 +51,11 @@ function parse(html) {
   // 从dt开始遍历dom树，生成对象
   var obj = foo($dt);
 
-  // 将对象转化为json字符串，添加额外参数使json格式更易阅读
-  var s = JSON.stringify(obj, null, 4);
-  // 将json字符串写入json文件
-  fs.writeFileSync("bookmarks.json", s);
+  // // 将对象转化为json字符串，添加额外参数使json格式更易阅读
+  // var s = JSON.stringify(obj, null, 4);
+  // // 将json字符串写入json文件
+  // fs.writeFileSync("bookmarks.json", s);
+  return obj
 }
 
 function foo($dt) {
@@ -35,7 +66,7 @@ function foo($dt) {
     // a标签为网址
     var $a = $dt.children("a");
     // 返回该书签的名称和网址组成的对象
-    return $a.length > 0 ? { name: $a.text(), href: $a.attr("href"), icon: $a.attr("icon") } : null;
+    return $a.length > 0 ? { type: 2, name: $a.text(), href: $a.attr("href"), icon: $a.attr("icon") } : null;
   }
 
   var h3 = $h3.text();
@@ -54,6 +85,7 @@ function foo($dt) {
   }
 
   // 创建文件夹与子文件数组的键值对
+  obj.type = 1;
   obj.name = h3;
   obj.children = arr;
   // 返回该对象
